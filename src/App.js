22e709ee8b1f89da43beab2be3f14e23eadd366e
anirudh gautam <anirudhgautam987@gmail.com>
2022-05-04 23:10:25 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner.js";
 import Navbar from "./Navbar.js";
+import { SpinnerDotted } from "spinners-react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
     <div className="App">
-      <Navbar />
-      <Banner />
-      <Row
-        title="Trending"
-        fetchUrl={requests.fetchTrending}
-        isLargeRow={true} /*default true without anuything*/
-      />
       
-      <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Romantic movies" fetchUrl={requests.fetchRomanceMovies} />
+        {loading ? (
+        <SpinnerDotted
+          size={57}
+          thickness={100}
+          speed={100}
+          color="rgb(217, 18, 18)"
+          className="loader"
+          
+         
+        />
+       
+      ) 
+      : (
+        <>
+          <Navbar />
+          <Banner />
+          <Row
+            title="Trending"
+            fetchUrl={requests.fetchTrending}
+            isLargeRow={true}
+          />
 
-      <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Top Grossing" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Old Movies" fetchUrl={requests.fetchOldMovies}/>
+          <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
+          <Row title="Romantic movies" fetchUrl={requests.fetchRomanceMovies} />
+
+          <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies} />
+          <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies} />
+          <Row title="Top Grossing" fetchUrl={requests.fetchTopRated} />
+          <Row title="Old Movies" fetchUrl={requests.fetchOldMovies} />
+        </>
+      )}
+      
     </div>
   );
 }
