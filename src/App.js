9eb487e391a02870046e9f6e26a250ee23cadd7d
anirudh gautam <anirudhgautam987@gmail.com>
2022-10-23 01:03:21 +0530

    new routes added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/login" element={<Subscribe />} />
+      <Route path="/subscription" element={<Subscribe />} />
       </Routes>
 
     </div>
